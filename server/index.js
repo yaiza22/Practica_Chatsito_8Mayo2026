33b992233ba8847434ea2b6fc0e1b7bcff4032cc
yaiza22 <yaizas719@gmail.com>
2026-05-08 10:01:34 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const { createServer } = require("node:http")
+const { Server} = require("socket.io")
+
+const app = express()
+const server = createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*"
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("Cliente conectado.")
+    
+    // Mensaje a todos
+    //io.emit("Bienvenida", "Hola")
+    socket.emit("Bienvenida", "Bienvenido al chat.")
+
+    socket.on("mensaje", (mensaje) => {
+        console.log("Mensaje recibido: ", mensaje)
+    })
+})
+
+server.listen(3000, () => {
+    console.log("Corriendo")
+})
