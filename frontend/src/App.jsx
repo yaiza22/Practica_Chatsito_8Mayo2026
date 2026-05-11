936ba9958014a69383c4a4fe7510298a180a1eae
yaiza22 <yaizas719@gmail.com>
2026-05-11 08:57:47 -0500

    Ajuste de campo input para evitar reload en la página

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
+
 function App() {
+  const [inputMessage, setInputMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    // Lógica de cómo se envían los mensajes
+  }
+
   return (
     <div>
-      <form>
-        <input/>
-        <button> Enviar </button>
+      <form onSubmit={handleSubmit}>
+        <input onChange={(e) => setInputMessage(e.target.value)}/>
+        <button type="submit"> Enviar </button>
       </form>
     </div>
   )
